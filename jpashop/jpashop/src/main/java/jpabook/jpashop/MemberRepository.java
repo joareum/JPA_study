3bd10b85ac3db3bed46d3ec592ad00830aefa527
joareum <a_lme@naver.com>
2024-07-05 17:50:24 +0900

    h2

---
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    public long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+}

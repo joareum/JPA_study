69e341616d06a92368ea85ba1aff614caffd3a19
joareum <a_lme@naver.com>
2024-07-07 16:23:23 +0900

    db 연결 관련 수정

--- jpashop/jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -9,8 +9,12 @@ public class MemberRepository {
     @PersistenceContext
     private EntityManager em;
 
-    public long save(Member member){
+    public Long save(Member member){
         em.persist(member);
         return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
     }
 }
